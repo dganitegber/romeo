2fd2870d898e2e7e17508101e2abe686f799d23d
dganitegber <dganite@gmail.com>
2021-10-08 20:01:31 +0200

    added ts

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,15 @@
 <template>
-    <v-card> </v-card>
+    <v-card>
+        <h2>hello</h2>
+    </v-card>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import axios, { AxiosResponse } from "axios";
-
-    export default class Card extends Vue ({
-
-        }
+    import { Component, Vue } from "vue-property-decorator";
+    // import { component } from "vue/types/umd";
+    // import axios from "axios";
+    @Component
+    export default class Card extends Vue {}
 </script>
 
 <style></style>
