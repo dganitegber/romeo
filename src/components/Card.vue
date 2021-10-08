458d7ff26d39c24a3facf75a04d0b9a3f9002325
dganitegber <dganite@gmail.com>
2021-10-08 19:36:10 +0200

    added decorator and axios

---
+++ src/components/Card.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-card> </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import axios, { AxiosResponse } from "axios";
+
+    export default class Card extends Vue ({
+
+        }
+</script>
+
+<style></style>
